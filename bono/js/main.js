360b73f86482eb0fedd3dd2f248895b63ba433b5
ecm203 <edcamo20@gmail.com>
2020-05-28 14:18:20 -0500

    Bono

--- bono/js/main.js
+++ bono/js/main.js
@@ -15,12 +15,20 @@ $(document).ready(function () {
       confirmButtonText: "Aceptar",
     });
   });
-
-    $("#txtDni").on("change",function(){
-      
+  $(".help-2").on("click", function () {
+    Swal.fire({
+      title: "Intrucciones",
+      icon:"question",
+      html:
+        "<ul>"+
+        "<li>Para realizar la consulta te recomendamos tener tu dni fisico a la mano</li>"+
+        "<li>A continuación ingrese el numero de su dni</li>"+
+        "<li>Ingrese la fecha de emisión de su dni</li>"+
+        "<li>Haga clic en aceptar</li>"+
+        "</ul>",
+      confirmButtonColor: "#bf0909",
+      confirmButtonText: "Aceptar",
     });
   });
-
-
-
-
+  $("#txtDni").on("change", function () {});
+});
