50c4596e785e78e79f992776bcb4494719868d79
ecm203 <edcamo20@gmail.com>
2020-05-27 22:49:35 -0500

    listo

--- bono/js/main.js
+++ bono/js/main.js
@@ -1,4 +1,26 @@
 // --------------- Loadding Page ------------------
-// $(window).on("load", function () {
-//    $(".panel").fadeOut("slow");
-//   });
+$(window).on("load", function () {
+  $(".panel").fadeOut("slow");
+});
+
+$(document).ready(function () {
+  $("#btnhelp").on("click", function () {
+    Swal.fire({
+      title: "Ubica la fecha de emisión de tu dni",
+      imageUrl: "img/dni-mesa-trabajo.png",
+      imageWidth: 402,
+      imageHeight: 334,
+      imageAlt: "Custom image",
+      confirmButtonColor: "#bf0909",
+      confirmButtonText: "Aceptar",
+    });
+  });
+
+    $("#txtDni").on("change",function(){
+      
+    });
+  });
+
+
+
+
