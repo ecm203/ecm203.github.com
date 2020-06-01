bd6520ea8948d1047ff68a29b0365d026de430de
ecm203 <edcamo20@gmail.com>
2020-06-01 17:56:35 -0500

    loading change

--- afp/js/main.js
+++ afp/js/main.js
@@ -6,56 +6,94 @@ $(window).on("load", function () {
 $(document).ready(function () {
   Swal.fire({
     showCloseButton: true,
-    html:
-      "<p class='title-modal'> Las fechas establecidas tienen en cuenta el último dígito o letra del DNI de cada aportante, y obedece al siguiente criterio</p>" +
-      "<table class='table' style='font-size:15px'>" +
-      "<thead>" +
-      "<tr>" +
-      "<th scope='col'>Código verificador</th>" +
-      "<th scope='col'>Día de solicitud</th>" +
-      "</tr>" +
-      "</thead>" +
-      "<tbody>" +
-      "<tr>" +
-      "<td>1</td>" +
-      "<td>Días 20 y 21 de mayo</td>" +
-      "</tr>" +
-      "<tr>" +
-      "<td>2</td>" +
-      "<td>Días 22 y 25 de mayo</td>" +
-      "</tr>" +
-      "<tr>" +
-      "<td>3</td>" +
-      "<td>Días 26 y 27 de mayo</td>" +
-      "</tr>" +
-      "<tr>" +
-      "<td>4</td>" +
-      "<td>Días 28 y 29 de mayo</td>" +
-      "</tr>" +
-      "<tr>" +
-      "<td>5</td>" +
-      "<td>Días 1 y 2 de mayo</td>" +
-      "</tr>" +
-      "<tr>" +
-      "<td>6</td>" +
-      "<td>Días 3 y 4 de mayo</td>" +
-      "</tr>" +
-      "<tr>" +
-      "<td>7</td>" +
-      "<td>Días 5 y 8 de mayo</td>" +
-      "</tr>" +
-      "<tr>" +
-      "<td>8</td>" +
-      "<td>Días 9 y 10 de mayo</td>" +
-      "</tr>" +
-      "<tr>" +
-      "<td>9</td>" +
-      "<td>Días 11 y 12 de mayo</td>" +
-      "</tr>" +
-      "</tbody>" +
-      "</table>",
-      showConfirmButton: false
-  });
+    html: `<p class='title-modal'>Bienvenido</p>
+          <div class="small">
+          <p style="color: dodgerblue;"> Cronograma de Solicitudes</p>
+        <table class="table border ">
+          <thead>
+            <tr>
+              <th scope="col">Último dÍgito de tu documento de Identidad</th>
+              <th scope="col">Registra tu solicitud solo los siguientes días</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <th>0</th>
+              <td>19 o 19 de mayo</td>
+            </tr>
+            <tr>
+              <th>1</th>
+              <td>20 o 21 de mayo</td>
+            </tr>
+            <tr>
+              <th>2</th>
+              <td>22 o 25 de mayo</td>
+            </tr>
+            <tr>
+              <th>3</th>
+              <td>26 o 27 de mayo</td>
+            </tr>
+            <tr>
+              <th>4 4</th>
+              <td>28 o 29 de mayo</td>
+            </tr>
+            <tr>
+              <th>5 5</th>
+              <td>1 o 2 de junio</td>
+            </tr>
+            <tr>
+              <th>6 6</th>
+              <td>3 o 4 de junio</td>
+            </tr>
+            <tr>
+              <th>7 7</th>
+              <td>5 o 8 de junio</td>
+            </tr>
+            <tr>
+              <th>8</th>
+              <td>9 o 10 de junio</td>
+            </tr>
+            <tr>
+              <th>9</th>
+              <td>10 o 11 de junio</td>
+            </tr>
+            <tr>
+              <th>Letra u otro valor no numérico</th>
+              <td>18 o 19 de junio</td>
+            </tr>
+          </tbody>
+        </table>
+        <p class="small">Reguerda que solo puedes presentar una solicitud de retiro hasta el 25% de tu fondo.</p>
+           </div>
+       `,
+    confirmButtonText: "Continuar"
+  }).then((result) => {
+    if (result.value) {
+      Swal.fire({
+        showCloseButton: true,
+        html: `<p class='title-modal'>Si no tienes una cuenta bancaria, puedes abrir una cuenta en línea en alguno de estos bancos. Haz clic en la entidad bancaria de tu elección</p>
+          <div class="small">
+          <ul>
+            <lo style="text-align: start;">
+            <li><a href="" style="cursor:pointer; ">Banbif</a> </li>
+            <li><a href="" style="cursor:pointer; ">Banco del Comercio</a></li>
+            <li><a href="" style="cursor:pointer; ">Banco de la Nación</a></li>
+            </lo>
+          </ul>
+
+          <ul>
+            <lo style="text-align: start;">
+            <li><a href="" style="cursor:pointer; ">Banco Falabella</a> </li>
+            <li><a href="" style="cursor:pointer; ">Banco GNB</a></li>
+            <li><a href="" style="cursor:pointer; ">Banco Pichincha</a></li>
+            </lo>
+          </ul>
+        
+           </div>
+       `,confirmButtonText: "Continuar"
+      })
+    }
+  })
 
   $("#btnhelp").on("click", function () {
     Swal.fire({
