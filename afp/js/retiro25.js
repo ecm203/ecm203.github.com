6364467d356fc9d74df401041d6a5d7fdd5689e1
ecm203 <edcamo20@gmail.com>
2020-06-01 19:05:39 -0500

    retiro 25 .html added

---
+++ afp/js/retiro25.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+  Swal.fire({
+    icon: "info",
+    title: "Importante leer",
+    html: ` 
+    <ul>
+        <li>
+            <p>
+                
+            </p>
+        </li>
+        <li>
+            <P>
+
+            </P>
+        </li>
+    </ul>`,
+    showCloseButton: true,
+    confirmButton: true,
+  });
+});
