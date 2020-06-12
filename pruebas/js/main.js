b4d8edc8020d166947fd2a0b1996cd46fd7c048b
ecm203 <edcamo20@gmail.com>
2020-06-11 20:50:13 -0500

    finished login

--- pruebas/js/main.js
+++ pruebas/js/main.js
@@ -1,17 +1,68 @@
-$(document).ready(function(){ 
-    $(".menu-open").on('click',function(){
-        $(".menu-close").css("display","flex");
-        $(".principal-menu").css("display","block");
-        $("#main").addClass("main");
-       $(".menu-open").css("display","none");
-    })
+// function animar() {
+//   var animacion = $(".caja").hasClass("bounceInRight");
+//   if (animacion == true) {
+//     $(".caja").removeClass("bounceInRight red ");
+//     // $(".caja").addClass("bounce");
+//   }
+//   else{
+//     $(".caja").addClass("bounceInRight red");
+//   }
 
-    $(".menu-close").on('click',function(){
-        $(".menu-open").css("display","flex");
-        $(".principal-menu").css("display","none");
-        $(".menu-close").css("display","none");
-        $("#main").removeClass("main")
-     })
+// }
 
-     new WOW().init();  
-})
+
+$(document).ready(function(){
+
+  $(".yes").on('click',function(){
+      $("#txtPassword").removeAttr( "type");
+      $(".no").removeClass("d-none");
+      $(".yes").addClass("d-none");
+      
+     
+  });
+  $(".no").on('click',function(){
+      $("#txtPassword").attr( "type","password");
+      $(".yes").removeClass("d-none");
+      $(".no").addClass("d-none")
+      $(".yes").css('color','#000');
+  });
+      
+  
+  $(".register-show").on('click',function(){
+    
+    $(".panel-show").removeClass("right-panel")
+  $(".register").removeClass("d-none")
+  $(".login").addClass("d-none")
+ 
+  });
+});
+
+function Izquierda() {
+  $(".panel-show").removeClass("right-panel")
+  $(".register").removeClass("d-none")
+  $(".login").addClass("d-none")
+
+}
+function Derecha() {
+  $(".panel-show").addClass("right-panel")
+  $(".register").addClass("d-none")
+  $(".login").removeClass("d-none")
+}
+
+function validarEmail() {
+  var value = document.getElementById("txtEmail").value;
+  var input = document.getElementById("input-div");
+  var feedback = document.getElementById("input-feedback");
+  if (
+    /^(([^<>()[\]\.,;:\s@\"]+(\.[^<>()[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i.test(
+      value
+    )
+  ) {
+    feedback.style.display = "none";
+  } else {
+    // input.className = "animated shake";
+    feedback.textContent = "Formate de email incorrecto ejemplo: nombre@ejemplo.com";
+    feedback.style.display = "block";
+    feedback.className = "animated bounceInRight";
+  }
+}
