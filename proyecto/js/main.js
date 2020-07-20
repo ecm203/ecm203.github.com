12028a83a1663b294c60250ffe9c814d9288ce10
ecm203 <edcamo20@gmail.com>
2020-07-19 20:34:07 -0500

    slider

--- proyecto/js/main.js
+++ proyecto/js/main.js
@@ -291,3 +291,22 @@ window.onload = function(){
     body.classList.remove("over-load");
   });
 }
+
+
+/*Main container*/
+var mySwiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  }
+})
